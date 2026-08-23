87574aff6da2bf7c1a85063a564a712cce8925a8
naitikjain2010 <naitikjain2010@gmail.com>
2026-08-23 21:43:04 +0530

    updated

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -22,7 +22,6 @@ plugins {
     // START: FlutterFire Configuration
     id("com.google.gms.google-services") version "4.4.4" apply false
     // END: FlutterFire Configuration
-    id("org.jetbrains.kotlin.android") version "2.3.20" apply false
-}
+    }
 
 include(":app")
