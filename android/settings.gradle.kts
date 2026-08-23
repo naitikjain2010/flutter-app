32b8910fed34114a6cd9fe057e02223690ec7dcd
naitikjain2010 <naitikjain2010@gmail.com>
2026-08-23 21:56:07 +0530

    Update kotlin version to 2.3.20

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -22,6 +22,7 @@ plugins {
     // START: FlutterFire Configuration
     id("com.google.gms.google-services") version "4.4.4" apply false
     // END: FlutterFire Configuration
+     id("org.jetbrains.kotlin.android") version "2.3.20" apply false
     }
 
 include(":app")
