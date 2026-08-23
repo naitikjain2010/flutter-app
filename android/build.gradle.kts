22d46a2c0d90e0b1148b2eac08fd8f6943387270
naitikjain2010 <naitikjain2010@gmail.com>
2026-08-23 11:57:00 +0530

    Fix: Update to Kotlin DSL format (.kts) for Gradle configuration

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
-    id("com.android.application")apply false
-    id("dev.flutter.flutter-gradle-plugin") apply false
+    id("com.android.application") version "8.1.0" apply false
+    id("com.android.library") version "8.1.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.8.22" apply false
 }
 
 allprojects {
@@ -8,19 +9,4 @@ allprojects {
         google()
         mavenCentral()
     }
-}
-
-val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
-rootProject.layout.buildDirectory.value(newBuildDir)
-
-subprojects {
-    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
-    project.layout.buildDirectory.value(newSubprojectBuildDir)
-}
-subprojects {
-    project.evaluationDependsOn(":app")
-}
-
-tasks.register<Delete>("clean") {
-    delete(rootProject.layout.buildDirectory)
 }
